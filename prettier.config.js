78715099f914c5233de779a20c3919c9e1f6e0da
Nicolas <nicolas.durpoix82@gmail.com>
2026-03-21 21:21:36 +0100

    chore : update prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -2,9 +2,19 @@
 
 /** @type {import('prettier').Config} */
 const config = {
-  semi: false,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'crlf',
+  experimentalOperatorPosition: 'start',
+  experimentalTernaries: true,
+  jsxSingleQuote: true,
+  objectWrap: 'collapse',
+  plugins: ['prettier-plugin-tailwindcss'],
+  semi: true,
+  singleAttributePerLine: true,
   singleQuote: true,
-  trailingComma: "all",
-};
+  tabWidth: 4,
+  useTabs: true,
+}
 
 export default config;
