83ce4c9fc439918dc057f5d861b662a5ec5fbcda
Nicolas <nicolas.durpoix82@gmail.com>
2026-03-22 14:12:34 +0100

    feat : new logo & header app title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,18 @@ import { Link } from '@tanstack/react-router';
 export default function Header() {
 	return (
 		<>
-			<header className='flex items-center bg-gray-800 p-4 text-white shadow-lg'>
+			<header className='bg-secondary-foreground flex items-center p-4 text-white shadow-lg'>
 				<h1 className='ml-4 text-xl font-semibold'>
-					<Link to='/'>
+					<Link
+						to='/'
+						className='flex items-center gap-2'
+					>
 						<img
-							src='/tanstack-word-logo-white.svg'
-							alt='TanStack Logo'
-							className='h-10'
+							src='/doggo.png'
+							alt='Good coloc Logo'
+							className='h-10 rounded-[99px]'
 						/>
+						Good coloc
 					</Link>
 				</h1>
 			</header>
